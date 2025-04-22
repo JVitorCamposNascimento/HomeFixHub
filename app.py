2baa497b11b5381787c11be2da10f542b85e9eee
J Vitor Campos <91292787+JVitorCamposNascimento@users.noreply.github.com>
2025-04-22 00:40:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import os
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
@@ -55,4 +55,5 @@ def gerar_orcamento(descricao):
     return 100
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
