b80b4bc4bbd87169f6c750c47cabea9226adbf9f
J Vitor Campos <91292787+JVitorCamposNascimento@users.noreply.github.com>
2025-04-22 20:02:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name_)
+
+#rotas 
+@app.route("/")
+def homepage():
+ return "Meu Site No ar !"
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
+app.run()
